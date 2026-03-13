ddab9068818b5d4b402de32d77598209f30c2cec
svkorovin-jpg <svkorovin@gmail.com>
2026-03-13 00:29:43 +0000

    Add Google Analytics and SEO metadata

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,3 +1,10 @@
+declare global {
+  interface Window {
+    dataLayer: any[]
+    gtag: (...args: any[]) => void
+  }
+}
+
 export function trackEvent(event: string, data?: any) {
   if (typeof window !== "undefined" && window.dataLayer) {
     window.dataLayer.push({
@@ -6,3 +13,5 @@ export function trackEvent(event: string, data?: any) {
     })
   }
 }
+
+export {}
